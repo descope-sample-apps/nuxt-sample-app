5263485a65d1aa0174693bf0e296c5557e4b679a
mrunankpawar <mrunank.pawar@descope.com>
2025-10-01 13:36:31 -0700

    fixed packages

--- .vercel/output/functions/__nitro.func/chunks/app/_nuxt/page-styles.1b0ed921.mjs
+++
@@ -1,8 +0,0 @@
-const page_vue_vue_type_style_index_0_scoped_0f43f914_lang = "pre[data-v-0f43f914]{background-color:transparent;color:rgb(255 255 255/var(--tw-text-opacity));margin-bottom:.75rem;margin-top:.75rem;padding:.5rem}pre[data-v-0f43f914],pre span[data-v-0f43f914]{--tw-text-opacity:1}pre span[data-v-0f43f914]{color:rgb(74 222 128/var(--tw-text-opacity))}";
-
-const page_vue_vue_type_style_index_1_lang = "body,html{height:100%;margin:0;overflow:hidden;padding:0}";
-
-const pageStyles_1b0ed921 = [page_vue_vue_type_style_index_0_scoped_0f43f914_lang, page_vue_vue_type_style_index_1_lang];
-
-export { pageStyles_1b0ed921 as default };
-//# sourceMappingURL=page-styles.1b0ed921.mjs.map
--- .vercel/output/static/_nuxt/NavBar.4f04d460.js
+++
@@ -1,1 +0,0 @@
-import{a as f}from"./entry.c5fb6c59.js";export{f as default};
