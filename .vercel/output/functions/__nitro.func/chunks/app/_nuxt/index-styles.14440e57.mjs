d1e6e108ffda8eca50ec4efb6afaff30a845f49e
mrunankpawar <mrunank.pawar@descope.com>
2025-10-03 11:06:26 -0700

    added vercel files

---
+++ .vercel/output/functions/__nitro.func/chunks/app/_nuxt/index-styles.14440e57.mjs
@@ -0,0 +1,6 @@
+const index_vue_vue_type_style_index_0_scoped_f9be2c07_lang = "pre[data-v-f9be2c07]{--tw-bg-opacity:1;--tw-text-opacity:1;--tw-shadow:0 1px 3px 0 rgba(0,0,0,.1),0 1px 2px -1px rgba(0,0,0,.1);--tw-shadow-colored:0 1px 3px 0 var(--tw-shadow-color),0 1px 2px -1px var(--tw-shadow-color);background-color:rgb(31 41 55/var(--tw-bg-opacity));border-radius:.25rem;box-shadow:0 0 #0000,0 0 #0000,var(--tw-shadow);box-shadow:var(--tw-ring-offset-shadow,0 0 #0000),var(--tw-ring-shadow,0 0 #0000),var(--tw-shadow);color:rgb(255 255 255/var(--tw-text-opacity));margin-bottom:.75rem;margin-top:.75rem;overflow-x:auto;padding:.75rem}pre span[data-v-f9be2c07]{--tw-text-opacity:1;color:rgb(74 222 128/var(--tw-text-opacity))}";
+
+const indexStyles_14440e57 = [index_vue_vue_type_style_index_0_scoped_f9be2c07_lang];
+
+export { indexStyles_14440e57 as default };
+//# sourceMappingURL=index-styles.14440e57.mjs.map
---
+++ .vercel/output/functions/__nitro.func/index.mjs
@@ -0,0 +1,16 @@
+globalThis._importMeta_={url:import.meta.url,env:process.env};import 'node-fetch-native/polyfill';
+import './chunks/nitro/config.mjs';
+import 'ufo';
+export { v as default } from './chunks/nitro/vercel.mjs';
+import 'destr';
+import 'cookie-es';
+import 'scule';
+import 'ofetch';
+import 'unenv/runtime/fetch/index';
+import 'hookable';
+import 'ohash';
+import 'unstorage';
+import 'defu';
+import 'next-auth/core';
+import 'requrl';
+//# sourceMappingURL=index.mjs.map
