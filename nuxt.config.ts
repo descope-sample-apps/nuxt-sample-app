0523d0efe12693791e02c94e192d884d25b232cb
mrunankpawar <mrunank.pawar@descope.com>
2025-10-03 09:50:57 -0700

    updated the nuxt config file

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,9 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
+    nitro: {
+        preset: 'vercel',
+        // Ensure you're using a compatible Node version
+    },
     css: [
         '@fortawesome/fontawesome-free/css/all.min.css'
     ],
